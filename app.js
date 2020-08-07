938a53c4f978ac35e4cb02a183b6fe0de3c1ced4
PriyanshiGupta <guptapriyanshi7796.pg@gmail.com>
2020-08-07 14:24:36 +0530

    add serverjs, app.js and nodemodule

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var bodyParser = require('body-parser');
+var path = require('path');
+var hsts = require('hsts');
+const app= express();
+app.set("view engine", "ejs");
+let ejs = require('ejs');
+const router =express.Router;
+
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb', parameterLimit: 50000  }));
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(express.static(__dirname + "/public"));
+
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Strict-Transport-Security: max-age=5184000');
+  next();
+});
+
+app.use(hsts({ maxAge: 5184000 }));
+    
+        
+        
+app.get('/', function(req, res){
+        res.render("homee.ejs");
+    });
+
+
+
+
+
+
+
+module.exports = app;
